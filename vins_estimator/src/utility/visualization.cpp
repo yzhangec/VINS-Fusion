b1d8aae749a4524c585ecf45e1287faa1e148de4
Eason Zhang <yzhangec@connect.ust.hk>
2022-06-25 13:57:33 +0800

    change message type for ral 2023

--- vins_estimator/src/utility/visualization.cpp
+++ vins_estimator/src/utility/visualization.cpp
@@ -39,7 +39,7 @@ void registerPub(ros::NodeHandle &n)
     pub_point_cloud = n.advertise<sensor_msgs::PointCloud>("point_cloud", 1000);
     pub_margin_cloud = n.advertise<sensor_msgs::PointCloud>("margin_cloud", 1000);
     pub_key_poses = n.advertise<visualization_msgs::Marker>("key_poses", 1000);
-    pub_camera_pose = n.advertise<nav_msgs::Odometry>("camera_pose", 1000);
+    pub_camera_pose = n.advertise<geometry_msgs::TransformStamped>("camera_pose", 1000);
     pub_camera_pose_visual = n.advertise<visualization_msgs::MarkerArray>("camera_pose_visual", 1000);
     pub_keyframe_pose = n.advertise<nav_msgs::Odometry>("keyframe_pose", 1000);
     pub_keyframe_point = n.advertise<sensor_msgs::PointCloud>("keyframe_point", 1000);
@@ -219,17 +219,17 @@ void pubCameraPose(const Estimator &estimator, const std_msgs::Header &header)
         Vector3d P = estimator.Ps[i] + estimator.Rs[i] * estimator.tic[0];
         Quaterniond R = Quaterniond(estimator.Rs[i] * estimator.ric[0]);
 
-        nav_msgs::Odometry odometry;
+        geometry_msgs::TransformStamped odometry;
         odometry.header = header;
         odometry.header.frame_id = "world";
-        odometry.pose.pose.position.x = P.x();
-        odometry.pose.pose.position.y = P.y();
-        odometry.pose.pose.position.z = P.z();
-        odometry.pose.pose.orientation.x = R.x();
-        odometry.pose.pose.orientation.y = R.y();
-        odometry.pose.pose.orientation.z = R.z();
-        odometry.pose.pose.orientation.w = R.w();
-
+        odometry.child_frame_id = "camera";
+        odometry.transform.translation.x = P.x();
+        odometry.transform.translation.y = P.y();
+        odometry.transform.translation.z = P.z();
+        odometry.transform.rotation.x = R.x();
+        odometry.transform.rotation.y = R.y();
+        odometry.transform.rotation.z = R.z();
+        odometry.transform.rotation.w = R.w();
         pub_camera_pose.publish(odometry);
 
         cameraposevisual.reset();
